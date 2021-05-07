3280583214ec46cb4c8b04928c1cf5ad0c287849
Wilma <wilkru-7@student.ltu.se>
2021-05-07 10:33:14 +0200

    Added events

---
+++ events/event.h
@@ -0,0 +1,15 @@
+//
+// Created by Wilma on 07/05/2021.
+//
+
+#ifndef D7049E_EVENT_H
+#define D7049E_EVENT_H
+
+#endif //D7049E_EVENT_H
+
+#pragma once
+
+class Event {
+public:
+    virtual int id();
+};
---
+++ events/keyboardEvent.h
@@ -0,0 +1,15 @@
+//
+// Created by Wilma on 07/05/2021.
+//
+
+#ifndef D7049E_KEYBOARDEVENT_H
+#define D7049E_KEYBOARDEVENT_H
+
+#include "event.h"
+
+class KeyboardEvent: public Event {
+public:
+    int KeyboardEvent::id() {return 1;}
+};
+
+#endif //D7049E_KEYBOARDEVENT_H
