063be6b7f9060c98e1f4442b445bf7596302fa49
carker-7 <carker-7@student.ltu.se>
2021-05-18 14:00:05 +0200

    inventory fix

--- objects/inventory.cpp
+++ objects/inventory.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "inventory.h"
+#include <iostream>
 
 Inventory::Inventory(std::list<Object*> *list, Object* object) {
     objects = list;
@@ -10,21 +11,33 @@ Inventory::Inventory(std::list<Object*> *list, Object* object) {
 }
 
 void Inventory::addToInventory(Object* item) {
-    item->physicsBody->setIsActive(false);
-    inventory.push_back(item);
-}
-
-void Inventory::pickFromInventory() {
-    Object* item = inventory.at(0);
-    item->physicsBody->setIsActive(true);
-
     rp3d::Transform ownerTransform = owner->physicsBody->getTransform();
     reactphysics3d::Vector3 ownerPos = ownerTransform.getPosition();
 
     rp3d::Transform itemTransform = item->physicsBody->getTransform();
-    itemTransform.setPosition(reactphysics3d::Vector3(ownerPos.x + 1, ownerPos.y, ownerPos.z + 1));
-    item->physicsBody->setTransform(itemTransform);
-    objects->push_back(item);
+    reactphysics3d::Vector3 itemPos = itemTransform.getPosition();
+    double distance = sqrt(pow(ownerPos.x - itemPos.x, 2.0) + pow(ownerPos.z - itemPos.z, 2.0));
+
+    if (distance < 5.0) {
+        item->physicsBody->setIsActive(false);
+        inventory.push_back(item);
+        objects->remove(item);
+    }
+}
+
+void Inventory::pickFromInventory() {
+    if (!inventory.empty()) {
+        Object* item = inventory.at(0);
+        item->physicsBody->setIsActive(true);
+
+        rp3d::Transform ownerTransform = owner->physicsBody->getTransform();
+        reactphysics3d::Vector3 ownerPos = ownerTransform.getPosition();
+
+        rp3d::Transform itemTransform = item->physicsBody->getTransform();
+        itemTransform.setPosition(reactphysics3d::Vector3(ownerPos.x + 1, ownerPos.y, ownerPos.z + 1));
+        item->physicsBody->setTransform(itemTransform);
+        objects->push_back(item);
+    }
 }
 
 void Inventory::update(int id) {
@@ -36,7 +49,6 @@ void Inventory::update(int id) {
             Object* item = objects->back();
             if(item->isPickabel){
                 addToInventory(item);
-                objects->remove(item);
                 std::cout << "Pick up" << std::endl;
             }
             break;
